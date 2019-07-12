8c56afe224b7ba87431a9ad2d91f8610a0e785d9
102092 <quaint3@gmail.com>
2019-07-12 17:50:45 +0900

    40일차

---
+++ 190712/실습/spring_aop/src/main/java/lab/spring/aop/service/UserService.java
@@ -0,0 +1,14 @@
+package lab.spring.aop.service;
+
+import java.util.List;
+
+import lab.spring.aop.persist.UserVO;
+
+public interface UserService {
+	public int registMember(UserVO vo);
+	public int updateUser(UserVO user);
+	public int removeUser(String uid);
+	public UserVO findUser(String uid);
+	public List<UserVO> login(String uid, String upwd);
+	public List<UserVO> findUserList();
+}
