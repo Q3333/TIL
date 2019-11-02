1fab293d4c2a3dc9df01ea9c4128627e974ed661
Q3333 <hil02047@gmail.com>
2019-11-02 19:37:40 +0900

    폴더정리

--- Python 자습/코드/1.모듈연습/6.Counter.py
+++
@@ -1,6 +0,0 @@
-import collections
- 
-myList = ['a', 'b', 'c', 'c', 'a', 'a']
-myCounter = collections.Counter(myList)
-print('myCounter:', myCounter)
-# myCounter: Counter({'a': 3, 'c': 2, 'b': 1})
--- Python 자습/코드/1.모듈연습/8.None.py
+++
@@ -1,8 +0,0 @@
-def three():
-    return 3
-
-def none():
-	return 
-
-print(three())
-print(none())
