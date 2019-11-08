0f51ea2e42a0158686592e5d546af3d0d3a5c304
Q3333 <hil02047@gmail.com>
2019-11-08 17:28:11 +0900

    hphk 3일차

---
+++ Python/Day3/README.md
@@ -0,0 +1,7 @@
+
+
+​	                                               ![image-20191108172752584](README.assets/image-20191108172752584.png)
+
+
+
+![image-20191108172741233](README.assets/image-20191108172741233.png)
--- Python/Day3/set_webhook.py
+++ Python/Day3/set_webhook.py
@@ -5,7 +5,8 @@ from pprint import pprint
 token = config('TOKEN')
 base_url = f"https://api.telegram.org/bot{token}"   
 
-url = "6a18a20e.ngrok.io"
+# url = "6a18a20e.ngrok.io"
+url ="Q333.pythonanywhere.com"
 setweb_url = f'/setwebhook?url={url}'
 
 req = requests.get(base_url+setweb_url).json()
