b303f2e902d22585acd2fccae441bdb10a955d0c
Q3333 <hil02047@gmail.com>
2019-11-08 16:36:39 +0900

    hphk 3일차

---
+++ Python/Day3/set_webhook.py
@@ -0,0 +1,13 @@
+from decouple import config
+import requests
+from pprint import pprint
+
+token = config('TOKEN')
+base_url = f"https://api.telegram.org/bot{token}"   
+
+url = "6a18a20e.ngrok.io"
+setweb_url = f'/setwebhook?url={url}'
+
+req = requests.get(base_url+setweb_url).json()
+
+pprint(req)
