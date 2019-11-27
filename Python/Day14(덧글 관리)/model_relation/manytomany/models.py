2c9e8dc20d220229363dd4d507c19e300af1fcf2
Q3333 <hil02047@gmail.com>
2019-11-27 17:25:40 +0900

    hphk 14일차

--- Python/Day14(덧글 관리)/model_relation/manytomany/models.py
+++ Python/Day14(덧글 관리)/model_relation/manytomany/models.py
@@ -11,14 +11,15 @@ class Person(models.Model):
 
 class Alcohol(models.Model):
     name = models.CharField(max_length=20)
-    people = models.ManyToManyField(Person, through='Sales', related_name="alcohols")
+    # people = models.ManyToManyField(Person, through='Sales', related_name="alcohols")
+    people = models.ManyToManyField(Person, related_name="alcohols")
 
     def __str__(self):
         return f'주류 NO.{self.id} : {self.name}'
 
-class Sales(models.Model):
-    person = models.ForeignKey(Person, on_delete=models.CASCADE)
-    alcohol = models.ForeignKey(Alcohol, on_delete=models.CASCADE)
+# class Sales(models.Model):
+#     person = models.ForeignKey(Person, on_delete=models.CASCADE)
+#     alcohol = models.ForeignKey(Alcohol, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return f'{self.person}이 마시는 {self.alcohol}'
+#     def __str__(self):
+#         return f'{self.person}이 마시는 {self.alcohol}'
