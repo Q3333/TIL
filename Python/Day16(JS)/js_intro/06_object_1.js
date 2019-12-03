47d82a271e725012f362480cde3f9b661863a5a5
Q3333 <hil02047@gmail.com>
2019-12-03 20:44:41 +0900

    hphk 16일차

---
+++ Python/Day16(JS)/js_intro/06_object_1.js
@@ -0,0 +1,25 @@
+var books = ['Learning JS', 'Learning Django']
+var comics = {
+  DC: ['AquaMan', 'SuperMan'],
+  Marvel: ['IronMan', 'AntMan']
+}
+
+var magazines = null;
+
+// before
+// var bookShop = {
+//   books: books,
+//   comics: comics,
+//   magazines: magazines
+// }
+
+// after
+let bookShop = {
+  books,
+  comics,
+  magazines,
+}
+
+console.log(bookShop)
+console.log(typeof bookShop)
+console.log(bookShop.books[0])
